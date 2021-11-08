f6a2c497c6b3cd307d154b9e27160e34b3b24ade
samerton <samerton@users.noreply.github.com>
2021-11-08 20:51:44 +0000

    Add Romanian translation
    Credit SuperHori

---
+++ upload/modules/Countdown/language/Romanian/countdown.php
@@ -0,0 +1,40 @@
+<?php
+/*
+ *	Made by Samerton
+ *  https://github.com/NamelessMC/Nameless/
+ *  NamelessMC version 2.0.0-pr12
+ *
+ *  License: MIT
+ *
+ *  Countdown language file
+ */
+
+$language = array(
+    'countdown' => 'Numărătoare inversă',
+    'countdown_name' => 'Nume',
+    'countdown_description' => 'Descriere',
+    'countdown_expires' => 'Expiră',
+    'no_countdown' => 'Nu a fost configurată încă o numărătoare inversă',
+    'updated_successfully' => 'Actualizat cu succes',
+    'configure_widget_info' => 'Puteți configura locația widget-ului în fila StaffCP -> Layout -> Widgets',
+
+    // Days/hours/minutes/seconds
+    'x_days' => '{x}z', // Don't replace {x}
+    'x_hours' => '{x}o', // Don't replace {x}
+    'x_minutes' => '{x}m', // Don't replace {x}
+    'x_seconds' => '{x}s', // Don't replace {x}
+
+    'countdown_format' => '{days}:{hours}:{minutes}:{seconds}', // Don't replace {days}, {hours}, {minutes}, {seconds}
+
+    // Validation
+    'name_required' => 'Vă rugăm să introduceți un nume',
+    'name_minimum' => 'Asigurați-vă că numele are cel puțin 2 caractere',
+    'name_maximum' => 'Asigurați-vă că numele are cel mult 256 de caractere',
+    'expires_required' => 'Vă rugăm să introduceți o dată de expirare'
+);
+
+/*
+ *  ROMANIAN LANGUAGE FILE
+ *    by SuperHori.Com
+ *
+ */
